a43976ec9a03d38f2365ea2a8753b352d87b0124
Frederick Adebayo <sprintcorp7@gmail.com>
2022-06-22 10:02:46 +0100

    create event page design

--- backend/middlewares/ResponseQuery.js
+++ backend/middlewares/ResponseQuery.js
@@ -14,33 +14,10 @@ const responseQuery = (model, populate = "") => async(req, res, next) => {
 	//Create query string
 	let queryStr = JSON.stringify(reqQuery);
 
-	//Create operators($gt, $gte, etc)
-	queryStr = queryStr.replace(
-		/\b(gt|gte|lt|lte|in)\b/g,
-		(match) => `$${match}`
-	);
 
 	//Finding resource
 	query = model.find(JSON.parse(queryStr));
 
-	//Select Fields
-	if (req.query.select) {
-		const fields = req.query.select.split(",").join(" ");
-		query = query.select(fields);
-	}
-
-	//Search Fields
-	if (req.query.search) {
-
-	}
-
-	//Sort Fields
-	if (req.query.sort) {
-		const sortBy = req.query.sort.split(",").join(" ");
-		query = query.sort(sortBy);
-	} else {
-		query = query.sort("-createdAt");
-	}
 
 	//Pagination
 	const page = parseInt(req.query.page, 10) || 1;
